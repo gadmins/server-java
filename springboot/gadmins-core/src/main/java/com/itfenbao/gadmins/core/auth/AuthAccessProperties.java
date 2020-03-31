ceeec69f2158ecb41c3f745f24c8fbe2dbcf7944
zhuzhe <zhuzhe@findpiano.cn>
2020-03-31 09:27:46 +0800

    fix

--- springboot/gadmins-core/src/main/java/com/itfenbao/gadmins/core/auth/AuthAccessProperties.java
+++ springboot/gadmins-core/src/main/java/com/itfenbao/gadmins/core/auth/AuthAccessProperties.java
@@ -3,13 +3,19 @@ package com.itfenbao.gadmins.core.auth;
 
 import com.itfenbao.gadmins.config.AppConfig;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
 
+/**
+ * @author itfenbao
+ */
 @ConfigurationProperties(
         prefix = "api-access"
 )
 public class AuthAccessProperties {
 
+    @NestedConfigurationProperty
     private AuthProperties admin = new AuthProperties(AppConfig.Access.ADMIN_TOKEN, AuthFrom.COOKIE);
+    @NestedConfigurationProperty
     private AuthProperties app = new AuthProperties(AppConfig.Access.APP_TOKEN, AuthFrom.HEADER);
 
     public AuthAccessProperties() {
